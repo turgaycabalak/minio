bcc145301ed40c91871c9fe522d73a474db3b997
Turgay Cabalak <tcabalak@trupt.com.tr>
2024-08-07 12:15:31 +0300

    style: code formatted.

--- src/main/java/com/document/document_service/DocumentServiceApplication.java
+++ src/main/java/com/document/document_service/DocumentServiceApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DocumentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DocumentServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DocumentServiceApplication.class, args);
+  }
 
 }
-// upload ve download endpointlerini geliştir
-// - upload ederken belirli dosya formatlarını kullansın
-// - upload ve download için işlem sınırı koy (session tutarak)
+
